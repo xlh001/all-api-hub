7dd35bf87363a3582796025a804ed340e2f7f36d
anime <street-anime-olive@duck.com>
2025-10-16 01:44:49 +0800

    refactor(i18n): reorganize translation structure and restore import/export section

--- utils/i18n.ts
+++ utils/i18n.ts
@@ -2,10 +2,9 @@ import i18n from "i18next"
 import LanguageDetector from "i18next-browser-languagedetector"
 import { initReactI18next } from "react-i18next"
 
+import enTranslation from "~/locales/en/translation.json"
+import zhCNTranslation from "~/locales/zh_CN/translation.json"
 import { userPreferences } from "~/services/userPreferences"
-
-import enTranslation from "../locales/en/translation.json"
-import zhCNTranslation from "../locales/zh_CN/translation.json"
 
 // Asynchronously load user's language preference
 const loadLanguagePreference = async () => {
