a11a1492b3acb9cf0084dea2ef08fdf7627c9958
anime <street-anime-olive@duck.com>
2025-10-24 03:15:26 +0800

    chore: update prettier config for Tailwind CSS support
    - extend config type to include Tailwind plugin options
    - add `prettier-plugin-tailwindcss` to plugins array

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,6 +1,4 @@
-/**
- * @type {import('prettier').Options}
- */
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 export default {
   printWidth: 80,
   tabWidth: 2,
@@ -10,7 +8,10 @@ export default {
   trailingComma: "none",
   bracketSpacing: true,
   bracketSameLine: true,
-  plugins: ["@ianvs/prettier-plugin-sort-imports"],
+  plugins: [
+    "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss"
+  ],
   importOrder: [
     "<BUILTIN_MODULES>", // Node.js built-in modules
     "<THIRD_PARTY_MODULES>", // Imports not matched by other special words or groups.
