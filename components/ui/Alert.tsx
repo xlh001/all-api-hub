0d4830cb8fcade5e62869a505ab812b830f45a95
anime <street-anime-olive@duck.com>
2025-10-24 01:48:47 +0800

    fix(Alert): correct ref types for AlertTitle and AlertDescription
    The AlertTitle ref type was changed from `HTMLParagraphElement` to
    `HTMLHeadingElement` to match the rendered `<h5>` element. The
    AlertDescription ref type was updated from `HTMLParagraphElement` to
    `HTMLDivElement` to match the rendered `<div>` element.

--- components/ui/Alert.tsx
+++ components/ui/Alert.tsx
@@ -89,7 +89,7 @@ const Alert = React.forwardRef<HTMLDivElement, AlertProps>(
 Alert.displayName = "Alert"
 
 const AlertTitle = React.forwardRef<
-  HTMLParagraphElement,
+  HTMLHeadingElement,
   React.HTMLAttributes<HTMLHeadingElement>
 >(({ className, ...props }, ref) => (
   <h5
@@ -101,7 +101,7 @@ const AlertTitle = React.forwardRef<
 AlertTitle.displayName = "AlertTitle"
 
 const AlertDescription = React.forwardRef<
-  HTMLParagraphElement,
+  HTMLDivElement,
   React.HTMLAttributes<HTMLParagraphElement>
 >(({ className, ...props }, ref) => (
   <div
