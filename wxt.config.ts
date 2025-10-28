60bb34179f898b681aa61204122d9d4cc4dcdec1
anime <street-anime-olive@duck.com>
2025-10-29 03:29:23 +0800

    chore: add 'alarms' permission to manifest configuration

--- wxt.config.ts
+++ wxt.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     name: "__MSG_manifest_name__",
     description: "__MSG_manifest_description__",
     default_locale: "zh_CN",
-    permissions: ["tabs", "storage", "sidePanel"],
+    permissions: ["tabs", "storage", "sidePanel", "alarms"],
     host_permissions: ["https://*/*"],
     browser_specific_settings: {
       gecko: {
