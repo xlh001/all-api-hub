fb175b7c9b24525c956b0355685d09e0acc0e2c1
anime <street-anime-olive@duck.com>
2025-10-28 17:24:37 +0800

    docs: update base path in vuepress config
    Remove `/all-api-hub/` prefix from the base URL to serve docs from root path.

--- docs/docs/.vuepress/config.js
+++ docs/docs/.vuepress/config.js
@@ -3,7 +3,7 @@ import { defaultTheme } from "@vuepress/theme-default"
 import { defineUserConfig } from "vuepress"
 
 export default defineUserConfig({
-  base: "/all-api-hub/",
+  base: "/",
 
   locales: {
     '/': {
